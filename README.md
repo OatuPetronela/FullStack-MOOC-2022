66e624d5d95ad6b72d012bd12c3ce282ea22cb7c
OatuPetronela <88310964+OatuPetronela@users.noreply.github.com>
2022-09-27 14:41:19 +0300

    Exercises 3.7-3.8

--- README.md
+++ README.md
@@ -57,3 +57,7 @@ Course material available at https://fullstackopen.com/
  - ✅ 3.2
  - ✅ 3.3
  - ✅ 3.4
+ - ✅ 3.5
+ - ✅ 3.6
+ - ✅ 3.7
+ - ✅ 3.8
--- part3/index.js
+++ part3/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
@@ -22,52 +23,52 @@ let persons = [
         "number": "39-23-6423122"
     }
 ]
-
+// get persons
 app.get("/api/persons", (req, res) => {
     res.json(persons)
 })
-
+// get info
 app.get("/api/info", (req, res) => {
     const data = new Date(Date.now()).toString()
     const total = `<h4> Phonebook has info for ${persons.length} people </br> ${data}</h4>`
     res.send(total)
 })
-
+// get every person after id
 app.get("/api/persons/:id", (req, res) => {
     const id = Number(req.params.id)
     const person = persons.find(person => person.id === id)
     if (person) {
         res.json(person)
     } else {
-        res
-            .status(404)
-            .end()
+        res.status(404).end()
     }
 })
 
+// add new person
 const generateId = () => {
     const maxId = persons.length > 0
         ? Math.max(...persons.map(person => person.id))
         : 0
     return maxId + 1;
 }
-
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 app.post("/api/persons", (req, res) => {
     const body = req.body;
-    if (!body.content) {
+    if (!body.name || !body.number) {
         return res
             .status(400)
-            .json({error: 'name must be unique'})
+            .json({error: 'you must add a nume and a number'})
     }
     const person = {
-        content: body.content,
-        important: body.important || false,
+        name: body.name,
+        number: body.number,
         id: generateId()
     }
+    console.log(person)
     persons = persons.concat(person)
     res.json(person)
 })
-
+// delete person
 app.delete("/api/persons/:id", (req, res) => {
     const id = Number(req.params.id);
     persons = persons.filter(person => person.id !== id)
@@ -75,6 +76,14 @@ app.delete("/api/persons/:id", (req, res) => {
         .status(204)
         .end();
 })
+// middleware after our routes, that is used for catching requests made to non-existent routes.
+const unknownEndpoint = (request, response) => {
+    response
+        .status(404)
+        .send({error: 'unknown endpoint'})
+}
+app.use(unknownEndpoint)
+
 
 const PORT = 3001
 app.listen(PORT, () => {
