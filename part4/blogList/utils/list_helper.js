e6ed0b32c3b94c0002a652685494054cce3b0ead
OatuPetronela <88310964+OatuPetronela@users.noreply.github.com>
2022-11-06 13:22:23 +0200

    Exercise 4.5

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -7,7 +7,20 @@
   const totalLikes =(blogs)=>{
     return blogs.reduce((sum, item) => sum + item.likes, 0);
   }
+
+  const favoriteBlog =(blogs)=>{
+
+    const max = blogs.reduce((prev, current)=>
+    prev.likes >current.likes ? prev : current
+    )
+    const favorite = {
+      title : max.title,
+      author: max.author,
+      likes: max.likes 
+    } 
+    return favorite;
+  }
   
   module.exports = {
-    dummy, totalLikes
+    dummy, totalLikes, favoriteBlog
   }
